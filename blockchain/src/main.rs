60b705e33189f658ab65bd2db736c4e1ea7c3d0b
matin <matin@qsix>
2023-12-03 22:30:56 +0330

    update readme

--- blockchain/src/main.rs
+++ blockchain/src/main.rs
@@ -1,6 +1,9 @@
 mod models;
 fn main() {
+   // Create a variable to store the difficulty of the blockchain
    let difficulty = 1;
+   // Create a new blockchain with the given difficulty
    let mut blockchain = models::blockchain::Blockchain::new(difficulty);
+   // Add a block to the blockchain
    models::blockchain::Blockchain::add_block(&mut blockchain);
 }
